303ab4a496b93c81b85b3599d47cc056079576f7
BrunoFerreiraLyra <brunocolab2024@gmail.com>
2026-03-10 15:03:14 -0300

    ajustando layout

--- fundo_e_borda/app/layout.tsx
+++ fundo_e_borda/app/layout.tsx
@@ -1,12 +1,32 @@
-export const metadata = {
-  title: "Meu Site",
-  description: "Projeto Next.js",
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Fundos e Bordas",
+  description: "exercicio de fundo e borda",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
-    <html lang="pt-br">
-      <body>
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         {children}
       </body>
     </html>
