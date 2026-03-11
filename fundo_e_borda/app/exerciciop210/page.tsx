7b00ded6c15b9bfe5334de5915c2254ef427cf56
BrunoFerreiraLyra <brunocolab2024@gmail.com>
2026-03-11 10:03:59 -0300

    p210

---
+++ fundo_e_borda/app/exerciciop210/page.tsx
@@ -0,0 +1,7 @@
+export default function Exerciciop210() {
+  return (
+    <main className="fundo-imagem">
+      <h1 className="titulo-destaque">Primeiro exercício de CSS</h1>
+    </main>
+  );
+}
--- fundo_e_borda/app/page.tsx
+++ fundo_e_borda/app/page.tsx
@@ -1,7 +1,14 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
-    <main>
-      <h1>Projeto em Branco</h1>
+    <main style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+      <h1>Atividade 10/03</h1>
+      <p>Escolha um dos links abaixo para navegar:</p>
+
+      <Link href="/exerciciop210" style={{ color: 'blue', textDecoration: 'underline' }}>
+        Ir para o Primeiro Exercício de CSS
+      </Link>
     </main>
   );
 }
