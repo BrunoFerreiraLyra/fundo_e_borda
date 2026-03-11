7f2422b6c4b95c2a70a9648cab9cdba0a952e1f0
BrunoFerreiraLyra <brunocolab2024@gmail.com>
2026-03-11 12:31:11 -0300

    links

--- fundo_e_borda/app/exerciciop246/page.tsx
+++ fundo_e_borda/app/exerciciop246/page.tsx
@@ -2,13 +2,17 @@ export default function Exerciciop246() {
   return (
     <main style={{ padding: '50px' }}>
       <p className="texto-sublinhado">
-        Texto sublinhado, paragrafo.
+        Texto sublinhado, paragrafo. CSS muito bom
       </p>
 
       <div className="meus-links">
-        <a href="#"> Hyperlink</a>
+        <a href="https://www.google.com" target="_blank" rel="noopener noreferrer"> 
+          Abrir Google
+        </a>
         <br /><br />
-        <a href="#"> Hyperlink</a>
+        <a href="/"rel="noopener noreferrer">
+          Voltar pra Página Inicial
+        </a>
       </div>
     </main>
   );
