6031d1d2fcaf711df4125a968cc29efe745c62fa
BrunoFerreiraLyra <brunocolab2024@gmail.com>
2026-03-11 12:10:53 -0300

    nw

--- fundo_e_borda/app/exerciciop231/page.tsx
+++ fundo_e_borda/app/exerciciop231/page.tsx
@@ -2,7 +2,7 @@ export default function Exerciciop231() {
   return (
     <main style={{ padding: '50px 0' }}>
       <div className="caixa-vermelha">
-       
+       <p> niga</p>
       </div>
       <br /> 
       <div className="caixa-verde">
