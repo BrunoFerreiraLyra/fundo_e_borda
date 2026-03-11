d20dedb635647f5bc3394fa40bb7a66dedbacf25
BrunoFerreiraLyra <brunocolab2024@gmail.com>
2026-03-11 11:15:58 -0300

    nt

--- fundo_e_borda/app/exerciciop231/page.tsx
+++ fundo_e_borda/app/exerciciop231/page.tsx
@@ -2,11 +2,11 @@ export default function Exerciciop231() {
   return (
     <main style={{ padding: '50px 0' }}>
       <div className="caixa-vermelha">
-        <p> DIV vermelha com 0px de padding.</p>
+       
       </div>
       <br /> 
       <div className="caixa-verde">
-        <p> DIV verde com 25px de padding.</p>
+        
       </div>
     </main>
   );
